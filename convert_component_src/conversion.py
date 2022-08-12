7e7d2dbb7da3dabbf74e80948303d3ee119bf2f1
Chloe Xie <t-xiechloe@microsoft.com>
2022-08-12 10:41:06 -0700

    Some changes to pipeline

--- convert_component_src/conversion.py
+++ convert_component_src/conversion.py
@@ -10,11 +10,12 @@ import torch
 import yaml 
 from yaml.loader import SafeLoader
 import json
-from sklearn.metrics import accuracy_score, precision_score, f1_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score
 import time
 
 import os
-print(os.environ)
+import matplotlib.pyplot as plt
+
 def read_load_model(model_folder):
     """
     returns the instance of the input MLflow model by reading its flavors.
@@ -54,37 +55,44 @@ def load_input_data(model_folder):
         
     return (X, y)
 
-def evaluate(model, premodel, X, y, threshold, metrics_list):
+def evaluate(model, premodel, X, y, threshold, num_runs, metrics_list):
     """
     Scores the new model on the input data and compares performance results.
     Fails self if perf is worse.
     Returns scored dataset
     """
     
-    n = 105 # TODO: how many runs? make it a parameter?
+    n = num_runs # TODO: an arbitrary number of runs but can parameterize in the future
 
-    # pre-conversion: time predict() with RandomForestClassifier and get accuracy score
+    # pre-conversion: time predict() with RandomForestClassifier 
     pre_conv = []
     for i in range(0, n):
         start = time.perf_counter()
         pred = premodel.predict(X)
         end = time.perf_counter()
-        if (n > 4): # omit first few runs, according to Klein
-            pre_conv.append(end - start)
-    pre_avg_ms = sum(pre_conv) / n * 1000
-
-    # pre-converion: log metrics
-    mlflow.log_metric('Pre-conversion-prediction-time (ms)', pre_avg_ms) 
-
-    # post conversion: time predict() with new model and get accuracy score
+        pre_conv.append(end - start)
+    
+    # post conversion: time predict() with new model 
     post_conv = []
     post_acc = []
     for i in range(0, n):
         start = time.perf_counter()
         pred_cpu_hb = model.predict(X)
         end = time.perf_counter()
-        if (n > 4): # omit first few runs, according to Klein
-            post_conv.append(end - start)
+        post_conv.append(end - start)
+    
+    # create box plots
+    fig = plt.boxplot([pre_conv, post_conv],patch_artist=True,labels=['pre-conv (ms)', 'post-conv (ms)']) 
+    plt.savefig('boxplot.png')   
+    mlflow.log_artifact('boxplot.png')
+
+    pre_conv = pre_conv[5:]
+    pre_avg_ms = sum(pre_conv) / n * 1000
+
+    # pre-converion: log metrics
+    mlflow.log_metric('Pre-conversion-prediction-time (ms)', pre_avg_ms) 
+    
+    post_conv = post_conv[5:]
     post_avg_ms = sum(post_conv) / n * 1000
 
     # post converion: log metrics
@@ -98,6 +106,14 @@ def evaluate(model, premodel, X, y, threshold, metrics_list):
     return pred_cpu_hb
 
 def metric_evaluation(metrics_list, y_true, y_pred, y_pred_cpu_hb):
+    """
+        Logs metrics that user inputs. Currently supported metrics are for classification only: accuracy, precision, and recall.
+        params:
+            metrics_list (str): string of metrics formatted like, "accuracy,precision,recall". 
+            y_true: a 2D ndarray of shape with each row representing one sample and each column representing the features.
+            y_pred: the second ndarray of shape containing the target samples.
+            y_pred_cpu_hb: the second ndarray of shape containing the target samples. 
+    """
     metrics_list = metrics_list.split(",")
     print(metrics_list)
         # go through list of metrics and evaluate
@@ -112,14 +128,14 @@ def metric_evaluation(metrics_list, y_true, y_pred, y_pred_cpu_hb):
             mlflow.log_metric('Pre-conversion-precision-score', precision)
             post_precision = precision_score(y_true, y_pred_cpu_hb)
             mlflow.log_metric('Post-conversion-precision-score', post_precision)
-        elif (metric == "f1"):
-            f1 = f1_score(y_true, y_pred)
-            mlflow.log_metric('Pre-conversion-f1-score', f1)
-            post_f1 = f1_score(y_true, y_pred_cpu_hb)
-            mlflow.log_metric('Post-conversion-f1-score', post-f1)
+        elif (metric == "recall"):
+            r = recall_score(y_true, y_pred)
+            mlflow.log_metric('Pre-conversion-recall_score', r)
+            post_r = recall_score(y_true, y_pred_cpu_hb)
+            mlflow.log_metric('Post-conversion-recall_score', post_r)
     return
 
-def convert(model_folder, target, threshold, metrics_list):
+def convert(model_folder, target, threshold, metrics_list, num_runs, output):
     """
     Converts an MLflow model to a target framework and returns an MLflow model in the target framework.
     
@@ -143,16 +159,16 @@ def convert(model_folder, target, threshold, metrics_list):
         pred = model.predict(X)
         sig = mlflow.models.infer_signature(X, pred)
 
-        scored_dataset = evaluate(model, premodel, X, y, threshold, metrics_list)
-        mlflow.onnx.log_model(model.model, 'onnx_model', input_example={"train": X, "test": y}, signature=sig)
+        scored_dataset = evaluate(model, premodel, X, y, threshold, num_runs,  metrics_list)
+        mlflow.onnx.save_model(model.model, path=output, input_example={"train": X, "test": y}, signature=sig)
     elif (target == "torch" or target == "pytorch"):
         model = hb_convert(premodel, 'torch')
         model.to('cuda')
         pred = model.predict(X)
         sig = mlflow.models.infer_signature(X, pred)
 
-        scored_dataset = evaluate(model, premodel, X, y, metrics_list)
-        mlflow.pytorch.log_model(model.model, 'torch_model', input_example={"train": X, "test": y}, signature=sig)
+        scored_dataset = evaluate(model, premodel, X, y, threshold, num_runs, metrics_list)
+        mlflow.pytorch.save_model(model.model, path=output, input_example={"train": X, "test": y}, signature=sig)
 
     return (model, scored_dataset)
 
@@ -163,6 +179,7 @@ parser.add_argument("--conversion_component_input", type=str)
 parser.add_argument("--conversion_target_input", type=str)
 parser.add_argument("--conversion_threshold_input", type=int)
 parser.add_argument("--conversion_metrics_input", type=str)
+parser.add_argument("--conversion_runs_input", type=int)
 parser.add_argument("--conversion_component_output", type=str)
 
 args = parser.parse_args()
@@ -172,7 +189,7 @@ print("conversion_target_input: %s" % args.conversion_target_input)
 print("conversion_component_output path: %s" % args.conversion_component_output)
 
 
-model = convert(args.conversion_component_input, args.conversion_target_input, args.conversion_threshold_input, args.conversion_metrics_input)
+model = convert(args.conversion_component_input, args.conversion_target_input, args.conversion_threshold_input, args.conversion_metrics_input, args.conversion_runs_input, args.conversion_component_output)
 
 
 
